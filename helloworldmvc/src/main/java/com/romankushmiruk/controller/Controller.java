b958ab46ec8759699b997757bd7ab9e00f4ae247
Roman <rKushmiruk@gmail.com>
2017-05-12 15:06:44 +0300

    Refactoring in helloworldmvc task

--- helloworldmvc/src/main/java/com/romankushmiruk/controller/Controller.java
+++ helloworldmvc/src/main/java/com/romankushmiruk/controller/Controller.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  * Created by roman on 11.05.17.
  */
 public class Controller {
-    private ResourceBundle stringConstants = ResourceBundle.getBundle("StringConstants");
+
     private Model model;
     private View view;
 
@@ -23,24 +23,24 @@ public class Controller {
     public void processUser() {
         Scanner sc = new Scanner(System.in);
         //input
-        inputWordFromConsole(stringConstants.getString("HELLO_WORD"), sc);
-        inputWordFromConsole(stringConstants.getString("WORLD_WORD"), sc);
+        inputWordFromConsole(view.HELLO_WORD, sc);
+        inputWordFromConsole(view.WORLD_WORD, sc);
 
         //Output
-        view.printMessageAndData(stringConstants.getString("OUTPUT_MESSAGE"), model.getMessage());
+        view.printMessageAndData(view.OUTPUT_MESSAGE, model.getMessage());
     }
 
     // The Utility method
     private void inputWordFromConsole(String msg, Scanner sc) {
-        view.printMessage(stringConstants.getString("INPUT_MESSAGE"));
+        view.printMessage(view.INPUT_MESSAGE);
         while (sc.hasNextLine()) {
             String result = sc.nextLine();
             if (result.compareToIgnoreCase(msg) == 0) {
                 model.addMessage(result);
-                view.printMessage(stringConstants.getString("SUCCESS_MESSAGE"));
+                view.printMessage(view.SUCCESS_MESSAGE);
                 return;
             } else {
-                view.printMessage(stringConstants.getString("WRONG_INPUT_DATA"));
+                view.printMessage(view.WRONG_INPUT_DATA);
             }
         }
     }
---
+++ helloworldmvc/src/test/java/com/romankushmiruk/model/ModelTest.java
@@ -0,0 +1,23 @@
+package com.romankushmiruk.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by roman on 12.05.17.
+ */
+public class ModelTest {
+    @Test
+    public void addMessage() throws Exception {
+    }
+
+    @Test
+    public void getMessage() throws Exception {
+    }
+
+    @Test
+    public void setMessage() throws Exception {
+    }
+
+}
